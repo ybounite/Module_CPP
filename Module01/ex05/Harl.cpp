6e5358a685b43473e2edc01ce7dd6736b0c22eba
yossef-cod <dimayoussef42@gmail.com>
2025-09-22 16:05:03 -0400

    [200~ Implemented Harl::complain(std::string level) to call the correct log method

---
+++ Module01/ex05/Harl.cpp
@@ -0,0 +1,49 @@
+#include "Harl.hpp"
+
+Harl::Harl( void )
+{
+}
+
+Harl::~Harl( void )
+{
+}
+
+void		Harl::debug( void )
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void		Harl::info( void )
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void		Harl::warning( void )
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void		Harl::error( void )
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void		Harl::complain( std::string level )
+{
+	std::string	levels[] = { "debug", "info", "warning", "error" };
+	// Define an array of pointers to member functions
+	void (Harl::*FunPtr[])() = {
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error
+	};
+
+	for (short i = 0; i < 4; i++)
+	{
+		if (levels[i] == level){
+			(this->*FunPtr[i])();
+			return ;
+		}
+	}
+}
