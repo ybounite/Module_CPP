62b035f9dc963e6dde4541b08cc30941f28bfc28
ybounite <dimayoussef42@gmail.com>
2025-10-04 15:32:49 +0100

    add get name members function

--- Module01/ex01/zombieHorde.cpp
+++ Module01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 14:06:42 by ybounite          #+#    #+#             */
-/*   Updated: 2025/09/11 16:29:48 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/10/04 14:45:06 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ Zombie*	zombieHorde( int N, std::string name )
 	Zombie* Horde = new Zombie[N];
 
 	for (int i = 0; i < N; i++)
-		Horde[i] = Zombie( name );
+		Horde[i].setName( name );
 	return Horde; 
 }
