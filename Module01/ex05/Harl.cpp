77634dbe218e66047874b3205bef739ec647dab1
ybounite <dimayoussef42@gmail.com>
2025-10-04 15:35:55 +0100

    print message enUnknown

--- Module01/ex05/Harl.cpp
+++ Module01/ex05/Harl.cpp
@@ -10,28 +10,32 @@ Harl::~Harl( void )
 
 void		Harl::debug( void )
 {
+	std::cout << "DEBUG\n";
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
 
 void		Harl::info( void )
 {
+	std::cout << "INFO\n";
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void		Harl::warning( void )
 {
+	std::cout << "WARNING\n";
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void		Harl::error( void )
 {
+	std::cout << "ERROR\n";
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void		Harl::complain( std::string level )
 {
 	std::string	levels[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
-	// Define an array of pointers to member functions
+
 	void (Harl::*FunPtr[])() = {
 		&Harl::debug,
 		&Harl::info,
@@ -46,4 +50,5 @@ void		Harl::complain( std::string level )
 			return ;
 		}
 	}
+	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 }
