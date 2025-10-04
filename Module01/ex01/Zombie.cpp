b888b2ee2415834d346d43d60d00dd4f46d7219f
ybounite <dimayoussef42@gmail.com>
2025-10-04 15:33:01 +0100

    add get name members function

--- Module01/ex01/Zombie.cpp
+++ Module01/ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 14:05:50 by ybounite          #+#    #+#             */
-/*   Updated: 2025/09/11 15:30:42 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/10/04 14:57:08 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,17 @@
 
 Zombie::Zombie() {}
 
-Zombie::Zombie( std::string name )
-	: _name( name ) {}
-
 Zombie::~Zombie()
 {
-
-	std::cout << getName() << " is destroyed 💀" << std::endl;
+	std::cout << "Zombie " << getName() << " is destroyed 💀" << std::endl;
 }
 
-std::string	Zombie::getName( void ) { return Zombie::_name; }
+void		Zombie::setName( std::string name ) { this->_name = name; }
+
+std::string	Zombie::getName( void ) { return this->_name; }
 
 void		Zombie::announce( void )
 {
-
 	std::cout <<  getName() << ": BraiiiiiiinnnzzzZ...\n";
 }
 
