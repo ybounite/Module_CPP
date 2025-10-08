d4df5774429f808db38471fc703f4017f0ea9e3a
ybounite <dimayoussef42@gmail.com>
2025-10-08 09:25:32 +0100

    Replace include guards with #pragma once in Fixed class

--- Module02/ex00/Fixed.hpp
+++ Module02/ex00/Fixed.hpp
@@ -1,7 +1,16 @@
-#pragma once
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 14:34:30 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/07 14:34:33 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-# ifndef __FIXED_H__
-# define __FIXED_H__
+#pragma once
 
 # include <iostream>
 # include <string>
@@ -19,13 +28,11 @@ public:
 	Fixed & operator=( const Fixed &Other );
 	~Fixed( void );
 
-	int		getRawBits( void ) const;
+	int	getRawBits( void ) const;
 	void	setRawBits( int const raw );
 
 private:
 
-	int					_value;
+	int			_value;
 	static const int	_FractionalBits;
 };
-
-#endif
