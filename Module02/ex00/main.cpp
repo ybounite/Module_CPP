c23f9321861bb56851dd7b99c1ed12c41d643fb2
ybounite <dimayoussef42@gmail.com>
2025-10-08 09:23:44 +0100

    copy constructor b = a and b( a ) copy assigment operator b = a and b.operator=(a)

--- Module02/ex00/main.cpp
+++ Module02/ex00/main.cpp
@@ -1,13 +1,33 @@
-#include "Fixed.hpp"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/06 16:13:42 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/07 14:57:43 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "Fixed.hpp"
 
 int main( void ) {
 
 	Fixed a;
-	Fixed b( a );
+	Fixed b ( a );
 	Fixed c;
-	c = b;
+
+	c = a;
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;return 0;
+	std::cout << c.getRawBits() << std::endl;
+	/*
+	 * copy constructor
+	 * b = a and b( a )
+	
+	 * copy assigment operator
+	 * b = a and b.operator=(a)
+	*/
+	return 0;
 }
