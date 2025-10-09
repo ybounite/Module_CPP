f31932119c0e1e6f9896d5c1a0ffa25318b60e5c
ybounite <dimayoussef42@gmail.com>
2025-10-09 09:58:25 +0100

    Implement copy assignment operator

--- Module02/ex00/Fixed.cpp
+++ Module02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 14:09:23 by ybounite          #+#    #+#             */
-/*   Updated: 2025/10/06 16:11:26 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/10/09 09:57:01 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,31 @@
 
 const int Fixed::_FractionalBits = 8;
 // default constructor 
-Fixed::Fixed( void ) : _value(0)
-{
+Fixed::Fixed( void ) : _value(0) {
 	std::cout << "Default constructor called" << std::endl;
 }
 // copy constructor
-Fixed::Fixed( const Fixed &other )
-{
+Fixed::Fixed( const Fixed &other ) {
 	std::cout << "Copy constructor called" << std::endl;
 	this->_value = other._value;
 }
 // copy assignment 
-Fixed & Fixed::operator=( const Fixed &Other )
-{
-	std::cout << "Copy assignment operator calle\n";
-	if (this != &Other)
-		this->_value = Other.getRawBits();
+Fixed & Fixed::operator=( const Fixed &Other ) {
+	std::cout << "Copy assignment operator calle" << std::endl;
+	this->_value = Other.getRawBits();
 	return *this;
 }
 // destructor
-Fixed::~Fixed( void )
-{
+Fixed::~Fixed( void ) {
 	std::cout << "Destructor called" << std::endl;
 }
 
-int		Fixed::getRawBits( void ) const
-{
-	std::cout << "getRawBits member function called\n";
+int	Fixed::getRawBits( void ) const	{
+	std::cout << "getRawBits member function called" << std::endl;
 	return _value;
 }
 
-void	Fixed::setRawBits( int const raw )
-{
+void	Fixed::setRawBits( int const raw ) {
 	this->_value = raw;
 }
 
