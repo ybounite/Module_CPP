fc22d639469b46f28e8e55214313952164ad316a
ybounite <dimayoussef42@gmail.com>
2025-10-09 09:55:26 +0100

    Implement test of this fixed-point

--- Module02/ex01/main.cpp
+++ Module02/ex01/main.cpp
@@ -1,18 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/09 09:29:51 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/09 09:47:32 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Fixed.hpp"
-
-
 
 int main( void ) {
 
 	Fixed a;
-	std::cout << "a is " << a << std::endl;
-	//Fixed const b( 10 );
-/*
+	Fixed const b( 10 );
 	Fixed const c( 42.42f );
 	Fixed const d( b );
 
-	a = Fixed( 1234.4321f );
-
+	a = Fixed( 1234.4321f );	
+	
 	std::cout << "a is " << a << std::endl;
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
@@ -22,6 +30,6 @@ int main( void ) {
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-*/	
+	
 	return 0;
 }
