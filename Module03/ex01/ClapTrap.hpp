f54ad133deab2fe97883af5d5653b4bc4d510bb5
ybounite <dimayoussef42@gmail.com>
2025-10-13 18:43:26 +0100

    Include private attributes for name, hit points, energy, and attack damage

---
+++ Module03/ex01/ClapTrap.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/12 11:49:19 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/12 13:07:17 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+# include <iostream>
+# include <string>
+// ************************************************************************** //
+//                               CLAPTRAP Class                               //
+// ************************************************************************** //
+
+class ClapTrap {
+
+
+public:
+
+	ClapTrap( void );
+	ClapTrap( std::string name );
+	ClapTrap( const ClapTrap &other );
+	ClapTrap & operator=( const ClapTrap &Other );
+	~ClapTrap( void );
+
+	void attack( const std::string& target );
+	void takeDamage( unsigned int amount );
+	void beRepaired( unsigned int amount );
+	std::string	getName( void ) const;
+
+private:
+	
+	std::string	_Name;
+	int		_HitPoints;
+	int		_EnergyPoints;
+	int		_AttackDamage;
+};
