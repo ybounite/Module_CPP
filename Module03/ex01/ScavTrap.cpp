27b60715fdb8d5fbc502e2a8c80e9db7afa30120
ybounite <dimayoussef42@gmail.com>
2025-10-15 09:18:22 +0100

    Implement ScavTrap class with constructors, destructor, and core methods

---
+++ Module03/ex01/ScavTrap.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/12 12:59:12 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/12 18:55:39 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap( void ) {
+
+	this->_HitPoints = 100;
+	this->_EnergyPoints = 50;
+	this->_AttackDamage = 20;
+	std::cout << "ScavTrap " << this->_Name << " constructor called" << std::endl;
+}
+
+ScavTrap::ScavTrap( std::string name ) : ClapTrap( name ){
+
+	std::cout << "ScavTrap constructor called" << std::endl;
+}
+
+ScavTrap::ScavTrap( const ScavTrap &Other ) : ClapTrap( Other){
+
+	std::cout << "ScravTrap copy constructor called" << std::endl;
+}
+
+ScavTrap	&ScavTrap::operator=( const ScavTrap &Other ) {
+
+	this->_Name = Other._Name;
+	this->_HitPoints = Other._HitPoints;
+	this->_EnergyPoints = Other._EnergyPoints;
+	this->_AttackDamage = Other._AttackDamage;
+	std::cout << "ScravTrap copy assignment operator called" << std::endl;
+	return *this;
+}
+
+ScavTrap::~ScavTrap( void ) {
+
+	std::cout << "ScavTrap destructor called" << std::endl;
+}
+
+void		ScavTrap::guardGate( void ) {
+
+	std::cout << "ScavTrap " << getName() << " has entered Get keeper mode!" << std::endl;
+}
