7ee2ade38ba16fc44d2f8bfd9f2b45306847d105
ybounite <dimayoussef42@gmail.com>
2025-10-15 11:11:46 +0100

    Implement ScavTrap class with constructors, destructor, and core methods

--- Module03/ex03/src/ScavTrap.cpp
+++ Module03/ex03/src/ScavTrap.cpp
@@ -20,14 +20,14 @@ ScavTrap::ScavTrap( void ) {
 	std::cout << "ScavTrap Default constructor called" << std::endl;
 }
 
-ScavTrap::ScavTrap( std::string name ) : ClapTrap( name ) {
+ScavTrap::ScavTrap( std::string name ) : ClapTrap( name ){
 
-	std::cout << "ScavTrap constructor called" << std::endl;
+	std::cout << "ScavTrap " << getName() << " is created" << std::endl;
 }
 
-ScavTrap::ScavTrap( const ScavTrap &Other ) : ClapTrap( Other) {
+ScavTrap::ScavTrap( const ScavTrap &Other ) : ClapTrap( Other ){
 
-	std::cout << "ScravTrap copy constructor called " << std::endl;
+	std::cout << "ScravTrap copy constructor called" << std::endl;
 }
 
 ScavTrap	&ScavTrap::operator=( const ScavTrap &Other ) {
@@ -42,10 +42,30 @@ ScavTrap	&ScavTrap::operator=( const ScavTrap &Other ) {
 
 ScavTrap::~ScavTrap( void ) {
 
-	std::cout << "ScavTrap " << getName() << " is destroyed 💀" << std::endl;
+	std::cout << "ScavTrap " << getName() << " is destructor" << std::endl;
+}
+
+void	ScavTrap::attack( const std::string& target ) {
+
+	std::cout << "ScavTrap " << getName();
+	if (this->_HitPoints <= 0) {
+		std::cout << " can't attack becouse it's destroyed!" << std::endl;
+		return ;
+	}
+	if (this->_EnergyPoints <= 0) {
+		std::cout << " hase no energy left to attack!" << std::endl;
+		return ;
+	}
+
+	std::cout << " attacks " + target + ", causing " << this->_AttackDamage
+		<< " points of damage!" << std::endl;
+	this->_EnergyPoints--;
 }
 
 void		ScavTrap::guardGate( void ) {
 
-	std::cout << "ScavTrap " << getName() << " has entered Get keeper mode!" << std::endl;
+	if(this->_EnergyPoints <= 0|| this->_HitPoints <= 0)
+		std::cout << "ScavTrap " << getName() << " cannot enter gate guard mode because " << getName() << " is dead." << std::endl;
+	else
+		std::cout << "ScavTrap " << getName() << " has entered Get keeper mode!" << std::endl;
 }
