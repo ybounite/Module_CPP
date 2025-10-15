935aefaec8a47c6523cc6c7f7a6c11489e96682a
ybounite <dimayoussef42@gmail.com>
2025-10-15 09:20:11 +0100

    Define ScavTrap class inheriting from ClapTrap

---
+++ Module03/ex01/ScavTrap.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/12 11:50:29 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/12 16:26:07 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#pragma once
+
+# include <iostream>
+# include <string>
+# include "ClapTrap.hpp"
+// ************************************************************************** //
+//                               SCAVTRAP Class                               //
+// ************************************************************************** //
+
+class ScavTrap : public ClapTrap {
+
+public:
+
+	ScavTrap( void );
+	ScavTrap( std::string name );
+	ScavTrap( const ScavTrap &Other );
+	ScavTrap	&operator=( const ScavTrap &Other );
+	~ScavTrap( void );
+
+	void		guardGate();
+};
