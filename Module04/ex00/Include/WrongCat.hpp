463e119b37def4390a20b88cf504f6d03fbc8b9f
ybounite <dimayoussef42@gmail.com>
2025-10-18 14:21:45 +0100

    implement WrongCat class inheriting from Animal with overridden makeSound()

---
+++ Module04/ex00/Include/WrongCat.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongCat.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 16:02:18 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/16 17:19:05 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+# include <iostream>
+# include <string>
+# include "WrongAnimal.hpp"
+// ************************************************************************** //
+//                               WRONGCAT Class                               //
+// ************************************************************************** //
+
+class WrongCat : public WrongAnimal {
+
+
+public:
+
+	WrongCat( void );
+	WrongCat( const WrongCat &other );
+	WrongCat & operator=( const WrongCat &Other );
+	~WrongCat( void );
+
+	void	makeSound( void );
+};
