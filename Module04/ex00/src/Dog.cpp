cdcd0b7a18d6c12e0fe8aef8bac7059370caa203
ybounite <dimayoussef42@gmail.com>
2025-10-18 14:33:42 +0100

    implement Dog class inheriting from Animal with overridden makeSound()

---
+++ Module04/ex00/src/Dog.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 18:13:52 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/15 18:58:33 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../Include/Dog.hpp"
+
+Dog::Dog( void ) {
+	setType("DOG");
+	std::cout << getType() << " constructors has been called\n";
+}
+
+Dog::Dog( const Dog &other ) : Animal( other ) {
+	std::cout << getType() << " copy constructor has been called" << std::endl;
+}
+
+Dog & Dog::operator=( const Dog &Other ) {
+	std::cout << getType() << " copy assignment has been called" << std::endl;
+	this->_type = Other._type;
+	return *this;
+}
+
+
+void	Dog::makeSound( void ) const {
+	std::cout << "Woof Woof Woof 🐕" << std::endl;
+}
+
+Dog::~Dog( void ) {
+	std::cout << getType() << " destructors has been called" << std::endl;
+}
