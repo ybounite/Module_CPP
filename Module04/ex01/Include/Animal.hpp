eefa9682836e469c40cb84f4ff0206426bedb282
ybounite <dimayoussef42@gmail.com>
2025-10-18 14:44:53 +0100

    implement Animal class

---
+++ Module04/ex01/Include/Animal.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 16:10:06 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/15 19:01:44 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+# include <iostream>
+# include <string>
+// ************************************************************************** //
+//                               ANIMAL Class                               //
+// ************************************************************************** //
+
+class Animal {
+
+
+public:
+
+	Animal( void );
+	Animal( const Animal &other );
+	Animal & operator=( const Animal &Other );
+	virtual ~Animal( void );
+
+	virtual void		makeSound( void ) const;
+	const std::string	getType( void ) const;
+	void			setType( const std::string type );
+
+protected:
+	std::string	_type;
+	
+};
