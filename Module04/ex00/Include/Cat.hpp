af04e7f71d6ba597ebfcc05c094003480cd4ab19
ybounite <dimayoussef42@gmail.com>
2025-10-18 10:21:26 +0100

    implement Cat class inheriting from Animal with overridden makeSound()

---
+++ Module04/ex00/Include/Cat.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 18:09:39 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/15 18:56:08 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+# include <iostream>
+# include <string>
+# include "Animal.hpp"
+// ************************************************************************** //
+//                               CAT Class                                    //
+// ************************************************************************** //
+
+class Cat : public Animal {
+
+
+public:
+
+	Cat( void );
+	Cat( std::string name );
+	Cat( const Cat &other );
+	Cat & operator=( const Cat &Other );
+	~Cat( void );
+
+
+	void	makeSound( void ) const;
+};
