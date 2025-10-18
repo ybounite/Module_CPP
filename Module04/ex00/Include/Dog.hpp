59edcf39dd49c73093d2bef70cc5385438dc69c9
ybounite <dimayoussef42@gmail.com>
2025-10-18 10:23:06 +0100

    implement dog class inheriting from Animal with overridden makeSound()

---
+++ Module04/ex00/Include/Dog.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 18:11:46 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/15 18:25:16 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+# include <iostream>
+# include <string>
+# include "Animal.hpp"
+// ************************************************************************** //
+//                               DOG Class                                    //
+// ************************************************************************** //
+
+class Dog : public Animal {
+
+
+public:
+
+	Dog( void );
+	Dog( std::string name );
+	Dog( const Dog &other );
+	Dog & operator=( const Dog &Other );
+	~Dog( void );
+
+	void	makeSound( void ) const;
+};
