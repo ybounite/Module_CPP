5a81f42aada8f09a73ebbb377fac2bff643221d8
ybounite <dimayoussef42@gmail.com>
2025-10-18 14:33:09 +0100

    implement CAt class inheriting from WrongAnimal with overridden makeSound()

---
+++ Module04/ex00/src/Cat.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 18:26:44 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/16 17:17:58 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../Include/Cat.hpp"
+
+Cat::Cat( void ) : Animal() {
+
+	this->setType("Cat");
+	std::cout << getType() << " constructors has been called\n";
+}
+
+Cat::Cat( const Cat &other ) : Animal( other ) {
+	std::cout << getType() << " copy constructor has been called" << std::endl;
+}
+
+Cat & Cat::operator=( const Cat &Other ) {
+	std::cout << getType() << " copy assignment has been called" << std::endl;
+	if (this != &Other)
+		this->_type = Other._type;
+	return *this;
+}
+
+Cat::~Cat( void ) {	
+	std::cout << getType() << " destructors has been called" << std::endl;
+}
+
+void	Cat::makeSound( void ) const {
+	std::cout << "Meow Meow Meow 🐱" << std::endl;
+}
+
+
