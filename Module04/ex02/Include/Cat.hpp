87897530d17aef0ca96951eb192baf2a0f9eb497
ybounite <dimayoussef42@gmail.com>
2025-10-20 16:22:37 +0100

    Implement Cat class with deep copy semantics and makeSound()

---
+++ Module04/ex02/Include/Cat.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 18:09:39 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/17 11:54:10 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+# include <iostream>
+# include <string>
+# include "Animal.hpp"
+# include "Brain.hpp"
+// ************************************************************************** //
+//                               CAT Class                                    //
+// ************************************************************************** //
+
+class Cat : public Animal {
+
+
+public:
+
+	Cat( void );
+	Cat( const Cat &other );
+	Cat & operator=( const Cat &Other );
+	~Cat( void );
+
+	void	makeSound( void ) const;
+
+private:
+	Brain*	_Brain;
+};
