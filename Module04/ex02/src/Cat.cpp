352918c8669f9c55b4e031129af37068e45667d3
ybounite <dimayoussef42@gmail.com>
2025-10-20 17:35:11 +0100

    Add Cat class to manage ideas with proper copy semantics

---
+++ Module04/ex02/src/Cat.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 18:26:44 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/16 17:17:58 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../Include/Cat.hpp"
+
+Cat::Cat( void ) : _Brain( new Brain()) {
+
+	this->setType("Cat");
+	this->_Brain->setIdeasIndex(100, this->_type);
+	std::cout << getType() << " Default constructors has been called" << std::endl;
+}
+
+Cat::Cat( const Cat &other ) : Animal( other ), _Brain( new Brain(*other._Brain)) {
+	std::cout << getType() << " copy constructor has been called" << std::endl;
+}
+
+Cat & Cat::operator=( const Cat &Other ) {
+	std::cout << getType() << " copy assignment has been called" << std::endl;
+	if (this != &Other)
+		this->_type = Other._type;
+	return *this;
+}
+
+Cat::~Cat( void ) {
+	delete _Brain;
+	std::cout << getType() << " destructors has been called" << std::endl;
+}
+
+void	Cat::makeSound( void ) const {
+
+	std::cout << "Meow Meow Meow 🐱" << std::endl;
+}
+
+
