8e6c80ae10faf350a7f6b960b88b321514b05a4b
ybounite <dimayoussef42@gmail.com>
2025-10-21 20:09:35 +0100

    header.hpp

---
+++ Module04/ex03/Include/Header.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Header.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 08:24:02 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/21 08:24:03 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+# include <iostream>
+# include <string>
+# include <vector>
+
+class ICharacter;
+class AMateria;
+
+
+# include "AMateria.hpp"
+# include "ICharacter.hpp"
+
+# include "Ice.hpp"
+# include "Cure.hpp"
+# include "Character.hpp"
+# include "IMateriaSource.hpp"
+// # include "MateriaSource.hpp"
