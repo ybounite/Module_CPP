d195ed3cdee1fe04f9808f0663d7cc028d932f84
ybounite <dimayoussef42@gmail.com>
2025-10-21 20:08:52 +0100

    Implement Inerface class

---
+++ Module04/ex03/Include/ICharacter.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 08:47:10 by ybounite          #+#    #+#             */
+/*   Updated: 2025/10/21 08:47:17 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# pragma once
+
+# include "Header.hpp"
+
+class ICharacter {
+
+public:
+
+	ICharacter();
+	ICharacter( const ICharacter &Other );
+	ICharacter &operator=( const ICharacter &Other);
+	virtual ~ICharacter();
+
+	virtual std::string const & getName() const = 0;
+	virtual void equip(AMateria* m) = 0;
+	virtual void unequip(int idx) = 0;
+	virtual void use(int idx, ICharacter& target) = 0;
+};
