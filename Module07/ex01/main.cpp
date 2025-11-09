a86665b975feaf5858be3eac691f6bbd3b2be64a
ybounite <dimayoussef42@gmail.com>
2025-11-09 12:54:47 +0100

    add main to test Template function iter functionality

--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -12,10 +12,23 @@
 
 # include "iter.hpp"
 
+void	print(int &arr) { std::cout << arr << " "; }
+
+void	printConst( const int &arr) {std::cout << arr << " "; }
+
+void	printString(const std::string &str ) { std::cout << str << std::endl; }
+
+void	print(const std::string &str){ std::cout << str << std::endl; }
+
 int	main() {
+	std::cout << "*** Array of integer ***" << std::endl;
 	int	intArray[] = {1, 2, 3, 4, 5};
-	std::string strarray[] = {"hello", "world", "c++", "templates"};
-	iter(intArray, 5, printElement<int>);
-	iter(strarray, 4 , printElement<std::string>);
+	iter(intArray, 5, printConst);
+
+	std::cout << "\n*** Template Array of string ***" << std::endl;
+	std::string strarray[] = {"hello", "world", "templates"};
+	iter(strarray, 3, printElement);
+	std::cout << "\n*** Array of string ***" << std::endl;
+	iter(strarray, 3, print);
 	return 0;
 }
