a41f34ccdcf44dbdae486541a3078ee6c7c0030d
ybounite <dimayoussef42@gmail.com>
2025-11-11 17:34:02 +0100

    Add main.cpp to test Span class functionality

---
+++ Module08/ex01/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                    file: main.cpp                                          */
+/*                    by: ybounite <ybounite@student.1337.ma>                 */
+/*                                                                            */
+/*                    Created: 2025/11/11 09:10 by ybounite                   */
+/*                    Updated: 2025/11/11 09:10 by ybounite                   */
+/* ************************************************************************** */
+
+# include "Span.hpp"
+int main()
+{
+	try {
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+	}
+	catch (std::exception &e){
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
+}
