0cb113f79de93aa239e1752b255ae32e178e6667
ybounite <dimayoussef42@gmail.com>
2025-11-21 15:26:12 +0100

    Add main function to initialize BitcoinExchange and load data from file

---
+++ Module09/ex00/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                    file: main.cpp                                          */
+/*                    by: ybounite <ybounite@student.1337.ma>                 */
+/*                                                                            */
+/*                    Created: 2025/11/16 17:56 by ybounite                   */
+/*                    Updated: 2025/11/16 17:56 by ybounite                   */
+/* ************************************************************************** */
+
+# include <iostream>
+# include "BitcoinExchange.hpp"
+
+int	main(int argc, char **argv) {
+	if (argc != 2) {
+		std::cerr << "Error: could not open file."<< std::endl;
+		return 1;
+	}
+	try {
+		BitcoinExchange Bit;
+		Bit.LoadDataFromFile();
+	}
+	catch (std::exception &e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
+	return 0;
+}
