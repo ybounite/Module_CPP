77d3c6f2c46fda965f461aa080c923e0aa35bca3
ybounite <dimayoussef42@gmail.com>
2025-12-08 11:57:27 +0100

    add main function with pmergeMe execution and error handling

---
+++ Module09/ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                    file: main.cpp                                          */
+/*                    by: ybounite <ybounite@student.1337.ma>                 */
+/*                                                                            */
+/*                    Created: 2025/11/17 15:08 by ybounite                   */
+/*                    Updated: 2025/11/17 15:08 by ybounite                   */
+/* ************************************************************************** */
+
+# include "PmergeMe.hpp"
+
+int	main(int arc, char **arv) {
+
+	if (arc == 1) {
+        	std::cerr << "Please Enter Some Arguments So I Have Something To Do";
+		std::endl(std::cerr);
+		return 1;
+	}
+	try {
+		PmergeMe	PmergeMe_;
+
+		PmergeMe_.hanleArgument(arv);
+	}catch ( std::exception &e ) {
+		std::cerr << "ERROR: " << e.what();
+		std::endl(std::cerr);
+		return 1;
+	}
+	return 0;
+}
